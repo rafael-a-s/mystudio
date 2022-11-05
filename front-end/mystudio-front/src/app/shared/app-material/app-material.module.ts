9c666acc34dc22a1f080e0ef3f790c160da2e78c
rafael-a-s <rafinha.rafaelaguiar@gmail.com>
2022-11-04 22:12:19 -0300

    Criando o layout inicial do projeto

---
+++ front-end/mystudio-front/src/app/shared/app-material/app-material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatGridListModule,
+  ]
+})
+export class AppMaterialModule { }
