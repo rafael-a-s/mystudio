91035a8c029aac2c96695ede4daa6f38dd2f349b
rafael-a-s <rafinha.rafaelaguiar@gmail.com>
2023-01-06 15:53:15 -0300

    fix: Regula usuário

---
+++ front-end/myStudio/src/app/usuario/criar-usuario/criar-usuario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-usuario',
+  templateUrl: './criar-usuario.component.html',
+  styleUrls: ['./criar-usuario.component.css']
+})
+export class CriarUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ front-end/myStudio/src/app/usuario/usuario.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CriarUsuarioComponent } from './criar-usuario/criar-usuario.component';
+
+
+
+@NgModule({
+  declarations: [
+    CriarUsuarioComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UsuarioModule { }
